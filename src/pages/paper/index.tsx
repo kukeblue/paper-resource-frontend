91fd96412f73735ecae8523f8c229315d5870b9f
DESKTOP-DR3IO37\chenhuan <821826173@qq.com>
2021-01-16 09:14:25 +0800

    ~

--- src/pages/paper/index.tsx
+++ src/pages/paper/index.tsx
@@ -14,30 +14,34 @@ interface Paper {
   file: string;
 }
 
-const {chHooks} = ChUtils
+const { chHooks } = ChUtils
 
 export default () => {
-  const { 
+  const {
     options: gradeOptions,
-    optionsMap: gradeOptionMap 
-  } = chHooks.useOptionFormListHook({url: 'http://api-paper.kukechen.top/api/grade/list'})
-  const { 
+    optionsMap: gradeOptionMap
+  } = chHooks.useOptionFormListHook({ url: 'http://api-paper.kukechen.top/api/grade/list' })
+  const {
     options: gradeStepOptions,
     optionsMap: gradeStepOptionMap
-   } = chHooks.useOptionFormListHook({url: 'http://api-paper.kukechen.top/api/gradeStep/list'})
-  
-   const { 
+  } = chHooks.useOptionFormListHook({ url: 'http://api-paper.kukechen.top/api/gradeStep/list' })
+
+  const {
     options: tagOptions,
     optionsMap: tagOptionMap
-   } = chHooks.useOptionFormListHook({url: 'http://api-paper.kukechen.top/api/tag/list'})
-  
+  } = chHooks.useOptionFormListHook({ url: 'http://api-paper.kukechen.top/api/tag/list' })
+
+  const {
+    options: subjectOptions,
+    optionsMap: subjectOptionMap
+  } = chHooks.useOptionFormListHook({ url: 'http://api-paper.kukechen.top/api/subject/list' })
 
   const columns = [
     {
       title: '试卷名称',
       dataIndex: 'name',
       key: 'name',
-      render: (text: string, record: Paper)=>{
+      render: (text: string, record: Paper) => {
         return <a target="_blank" href={"http://api-paperfile.kukechen.top/onlinePreview?url=" + record.file}>{text}</a>
       }
     },
@@ -50,15 +54,30 @@ export default () => {
       title: '类目',
       dataIndex: 'gradeId',
       key: 'gradeId',
-      render: (text: string, record: object) =>{
+      render: (text: string, record: object) => {
         return <span>{gradeOptionMap[text] && gradeOptionMap[text].name}</span>
+      }
+    },
+    {
+      title: '价格',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: '学科',
+      dataIndex: 'subjectId',
+      key: 'subjectId',
+      render: (text: string, record: Paper) => {
+        return <div>{record.tagIds.map(subjectId => {
+          return <span key={subjectId}>{subjectOptionMap[subjectId] && tagOptionMap[subjectId].name}</span>
+        })}</div>
       }
     },
     {
       title: '学期',
       dataIndex: 'gradeStepId',
       key: 'gradeStepId',
-      render: (text: string, record: object) =>{
+      render: (text: string, record: object) => {
         return <span>{gradeStepOptionMap[text] && gradeStepOptionMap[text].name}</span>
       }
     },
@@ -66,8 +85,8 @@ export default () => {
       title: '标签',
       dataIndex: 'tagIds',
       key: 'tagIds',
-      render: (text: string, record: Paper) =>{
-        return <div>{record.tagIds.map(tagId=>{
+      render: (text: string, record: Paper) => {
+        return <div>{record.tagIds.map(tagId => {
           return <span key={tagId}>{tagOptionMap[tagId] && tagOptionMap[tagId].name}</span>
         })}</div>
       }
@@ -76,114 +95,132 @@ export default () => {
   return (
     <div>
       <ChTablePanel
-            onEditBefore={(item)=>{
-                if(item.file instanceof Array) {
-                    item.file = item.file[0].response.result[0]
-                    const splitPath = item.file.split('/')
-                    item.fileName = splitPath[splitPath.length - 1]
-                    item.name = item.fileName.split('.')[0]
-                    item.fileType = item.fileName.split('.')[1]
-                    console.log('debug: 开始提交', item)
+        onEditBefore={(item) => {
+          if (item.file instanceof Array) {
+            item.file = item.file[0].response.result[0]
+            const splitPath = item.file.split('/')
+            item.fileName = splitPath[splitPath.length - 1]
+            item.name = item.fileName.split('.')[0]
+            item.fileType = item.fileName.split('.')[1]
+            console.log('debug: 开始提交', item)
+          }
+        }}
+        onEditFormat={(item: any) => {
+          item.file = []
+        }}
+        urlDelete='/api/paper/delete'
+        urlAdd='/api/paper/add'
+        urlUpdate='/api/paper/edit'
+        url='/api/paper/page'
+        columns={columns}
+        searchFormData={[
+          {
+            placeholder: "请输入名称",
+            layout: { span: 4 },
+            type: FormItemType.input,
+            label: '名称',
+            name: 'name',
+          },
+          {
+            placeholder: "请选择类目",
+            layout: { span: 4, offset: 1 },
+            type: FormItemType.select,
+            label: '类目',
+            name: 'gradeId',
+            options: gradeOptions,
+          }, {
+            placeholder: "请选择学期",
+            layout: { span: 4, offset: 1 },
+            type: FormItemType.select,
+            label: '学期',
+            name: 'gradeStepId',
+            options: gradeStepOptions,
+          }, {
+            placeholder: "请选择标签",
+            layout: { span: 4, offset: 1 },
+            type: FormItemType.multipleSelect,
+            label: '标签',
+            name: 'tagIds',
+            options: tagOptions,
+          }
+        ]}
+        formData={
+          [
+            {
+              type: FormItemType.upload,
+              label: '上传试卷',
+              name: 'file',
+              uploadurl: '/api/paper/upload',
+              uploadname: 'files',
+              itemshow: (editor: Paper) => {
+                if (editor.id) {
+                  return false
                 }
-            }}
-            onEditFormat={(item: any)=>{
-              item.file = []
-            }}
-            urlDelete='/api/paper/delete'
-            urlAdd='/api/paper/add'
-            urlUpdate='/api/paper/edit'
-            url='/api/paper/page'
-            columns={columns}
-            searchFormData={[
-              {
-                  placeholder: "请输入名称",
-                  layout: {span: 4},
-                  type: FormItemType.input,
-                  label: '名称',
-                  name: 'name',
+                return true
               },
-              {
-                placeholder: "请选择类目",
-                layout: {span: 4, offset: 1},
-                type: FormItemType.select,
-                label: '类目',
-                name: 'gradeId',
-                options: gradeOptions,
-              }, {
-                placeholder: "请选择学期",
-                layout: {span: 4, offset: 1},
-                type: FormItemType.select,
-                label: '学期',
-                name: 'gradeStepId',
-                options: gradeStepOptions,
-              }, {
-                placeholder: "请选择标签",
-                layout: {span: 4, offset: 1},
-                type: FormItemType.multipleSelect,
-                label: '标签',
-                name: 'tagIds',
-                options: tagOptions,
-              }
-            ]}
-            formData={
-              [
-                {
-                  type: FormItemType.upload,
-                  label: '上传试卷',
-                  name: 'file',
-                  uploadurl: '/api/paper/upload',
-                  uploadname: 'files',
-                  itemshow: (editor: Paper)=>{
-                    if(editor.id) {
-                      return false
-                    }
-                    return true
-                  },
-                  rules: [{ required: true, message: '文件上传失败', validator: 
-                    (rule, value, callback) => {
-                        try {
-                            if(value[0].response)  {
-                               if(value[0].response.status == 0) {
-                                 console.log('debug: 单文件上传成功！')
-                                 return Promise.resolve();
-                               }else {
-                                 callback('文件上传失败！');
-                               }
-                            }
-                        } catch (err) {
-                            callback(err);
+              rules: [{
+                required: true, message: '文件上传失败', validator:
+                  (rule, value, callback) => {
+                    try {
+                      if (value[0].response) {
+                        if (value[0].response.status == 0) {
+                          console.log('debug: 单文件上传成功！')
+                          return Promise.resolve();
+                        } else {
+                          callback('文件上传失败！');
                         }
+                      }
+                    } catch (err) {
+                      callback(err);
                     }
-                  }],
-                },  
-                {
-                  type: FormItemType.select,
-                  label: '类目',
-                  name: 'gradeId',
-                  options: gradeOptions,
-                  rules: [
-                    { required: true, message: '请输入类目名称' }
-                  ],
-                },{
-                  type: FormItemType.select,
-                  label: '学期',
-                  name: 'gradeStepId',
-                  options: gradeStepOptions,
-                  rules: [
-                    { required: true, message: '请输入学期名称' }
-                  ],
-                },{
-                  type: FormItemType.multipleSelect,
-                  label: '标签',
-                  name: 'tagIds',
-                  options: tagOptions,
-                  rules: [
-                    { required: true, message: '请选择标签' }
-                  ],
-                },
-              ]
-            }
-        />
+                  }
+              }],
+            },
+            {
+              type: FormItemType.input,
+              label: '价格',
+              name: 'price',
+              rules: [
+                { required: true, message: '请输入价格' }
+              ],
+            },
+            {
+              type: FormItemType.select,
+              label: '学科',
+              name: 'subjectId',
+              options: subjectOptions,
+              rules: [
+                { required: true, message: '请选择学科' }
+              ],
+            },
+            {
+              type: FormItemType.select,
+              label: '类目',
+              name: 'gradeId',
+              options: gradeOptions,
+              rules: [
+                { required: true, message: '请输入类目名称' }
+              ],
+            }, {
+              type: FormItemType.select,
+              label: '学期',
+              name: 'gradeStepId',
+              options: gradeStepOptions,
+              rules: [
+                { required: true, message: '请输入学期名称' }
+              ],
+            }, {
+              type: FormItemType.multipleSelect,
+              label: '标签',
+              name: 'tagIds',
+              options: tagOptions,
+              rules: [
+                { required: true, message: '请选择标签' }
+              ],
+            },
+          ]
+        }
+      />
     </div>
   );
 }
