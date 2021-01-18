7cb916c1d4aa5bc0fad432b44636f65e0310037f
821826173@qq.com <520201314wg>
2021-01-18 16:38:56 +0800

    ～

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,69 +1,75 @@
 import React, { useEffect, useState } from 'react';
-import { IRouteComponentProps } from 'umi'
+import { IRouteComponentProps } from 'umi';
 import { ChLayout } from 'ch-ui';
-import './index.less'
+import './index.less';
 
+export default function Layout({
+  children,
+  location,
+  route,
+  history,
+  match,
+}: IRouteComponentProps) {
+  const [currentPathIndex, setCurrentPathIndex] = useState(1);
+  const siderItemData = [
+    {
+      icon: <span className="paper_iconfont paper_iconnianjiguanli" />,
+      path: '/grade',
+      text: '年级管理',
+      click: () => {
+        history.push('/grade');
+      },
+    },
+    {
+      icon: <span className="paper_iconfont paper_iconshijuan" />,
+      path: '/paper',
+      text: '试卷管理',
+      click: () => {
+        history.push('/paper');
+      },
+    },
+    {
+      icon: <span className="paper_iconfont paper_iconpingtai_kemu" />,
+      path: '/subject',
+      text: '学科管理',
+      click: () => {
+        history.push('/subject');
+      },
+    },
+    {
+      icon: <span className="paper_iconfont paper_iconbiaoqian" />,
+      path: '/tag',
+      text: '标签管理',
+      click: () => {
+        history.push('/tag');
+      },
+    },
+  ];
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
+  useEffect(() => {
+    const index = siderItemData.findIndex((item) => {
+      return item.path == location.pathname;
+    });
+    if (index > -1) {
+      setCurrentPathIndex(index + 1);
+    }
+  }, [location.pathname]);
 
-export default function Layout({ children, location, route, history, match }: IRouteComponentProps) {
-
-    const [currentPathIndex, setCurrentPathIndex] = useState(1);
-    const siderItemData = [
-        {
-            icon: <span className='paper_iconfont paper_iconnianjiguanli' />,
-            path: '/grade',
-            text: '年级管理',
-            click: () => {
-                history.push('/grade')
-            },
-        },
-        {
-            icon: <span className='paper_iconfont paper_iconshijuan' />,
-            path: '/paper',
-            text: '试卷管理',
-            click: () => {
-                history.push('/paper')
-            },
-        },
-        {
-            icon: <span className='paper_iconfont paper_iconpingtai_kemu' />,
-            path: '/subject',
-            text: '学科管理',
-            click: () => {
-                history.push('/subject')
-            },
-        },
-        {
-            icon: <span className='paper_iconfont paper_iconbiaoqian' />,
-            path: '/tag',
-            text: '标签管理',
-            click: () => {
-                history.push('/tag')
-            },
-        }
-    ]
-    useEffect(() => {
-        const index = siderItemData.findIndex(item => {
-            return item.path == location.pathname
-        })
-        if (index > -1) {
-            setCurrentPathIndex(index + 1)
-        }
-    }, [location.pathname])
-
-
-    return <ChLayout
-        adminIcon={
-            <div className="flex-center">
-                <span className='paper_iconfont paper_iconxueyuan-fufeikecheng2'></span>
-            </div>
-        }
-        sider={{
-            siderItems: siderItemData,
-            currentItem: currentPathIndex
-        }}
-    >
-        <div className='layout-content'>
-            {children}
+  return (
+    <ChLayout
+      adminIcon={
+        <div className="flex-center">
+          <span className="paper_iconfont paper_iconxueyuan-fufeikecheng2"></span>
         </div>
+      }
+      sider={{
+        siderItems: siderItemData,
+        currentItem: currentPathIndex,
+      }}
+    >
+      <div className="layout-content">{children}</div>
     </ChLayout>
+  );
 }
--- src/pages/grade/index.tsx
+++ src/pages/grade/index.tsx
@@ -4,17 +4,14 @@ import { ChTablePanel, FormItemType } from 'ch-ui';
 import { Button, Tag } from 'antd';
 import { termType, TermType } from '@/config/common.data';
 
-
-
 export default () => {
-
   const columns = [
     {
       title: '类目',
       dataIndex: 'name',
       key: 'name',
     },
-  ]
+  ];
   const childColumns = [
     {
       title: '学期名称',
@@ -26,64 +23,64 @@ export default () => {
       dataIndex: 'term',
       key: 'term',
       render: (text: string) => {
-        return <Tag>{(termType as any)[text]}</Tag>
-      }
+        return <Tag>{(termType as any)[text]}</Tag>;
+      },
     },
-  ]
+  ];
   return (
     <div>
       <ChTablePanel
-        urlDelete='/api/grade/delete'
-        urlAdd='/api/grade/add'
-        urlUpdate='/api/grade/edit'
-        url='/api/grade/page'
+        urlDelete="/api/grade/delete"
+        urlAdd="/api/grade/add"
+        urlUpdate="/api/grade/edit"
+        url="/api/grade/page"
         columns={columns}
-        formData={
-          [{
+        formData={[
+          {
             type: FormItemType.input,
             label: '名称',
             name: 'name',
             rules: [{ required: true, message: '请输入年级名称' }],
-          }]
-        }
+          },
+        ]}
         expandable={{
-          expandedRowRender: (record: any) => <div>
-            <ChTablePanel
-              onEditBefore={(item: any) => {
-                item.gradeId = record.id
-                console.log(item);
-              }}
-              urlDelete='/api/gradeStep/delete'
-              urlAdd='/api/gradeStep/add'
-              urlUpdate='/api/gradeStep/edit'
-              url='/api/gradeStep/page'
-              query={{ gradeId: record.id }}
-              columns={childColumns}
-              formData={
-                [
+          expandedRowRender: (record: any) => (
+            <div>
+              <ChTablePanel
+                onEditBefore={(item: any) => {
+                  item.gradeId = record.id;
+                  console.log(item);
+                }}
+                urlDelete="/api/gradeStep/delete"
+                urlAdd="/api/gradeStep/add"
+                urlUpdate="/api/gradeStep/edit"
+                url="/api/gradeStep/page"
+                query={{ gradeId: record.id }}
+                columns={childColumns}
+                formData={[
                   {
                     type: FormItemType.input,
                     label: '名称',
                     name: 'name',
                     rules: [{ required: true, message: '请输入年级名称' }],
-                  }, {
+                  },
+                  {
                     type: FormItemType.radioGroup,
                     label: '学期',
                     name: 'term',
-                    rules: [{ required: true, message: '请选择学期' }],
-                    initialValue: 'UP',
+                    rules: [{ required: false, message: '请选择学期' }],
                     options: [
+                      { label: '默认', value: '' },
                       { label: '上学期', value: 'UP' },
                       { label: '下学期', value: 'DOWN' },
-                    ]
-                  }
-                ]
-              }
-            />
-          </div>,
-        }
-        }
+                    ],
+                  },
+                ]}
+              />
+            </div>
+          ),
+        }}
       />
     </div>
   );
-}
+};
